ca6d22835a2c8996cea0abe82243528cd20d04d7
Pedro Santos <pedrobsantos98@outlook.com>
2020-09-29 01:58:48 -0300

    Removed cmake files

--- Fila de Prioridade/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/Dev/C++ Programs/Projetos Visual Studio Code/Estruturas de Dados/Fila de Prioridade
-# Build directory: G:/Dev/C++ Programs/Projetos Visual Studio Code/Estruturas de Dados/Fila de Prioridade/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Pilha/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/Dev/C++ Programs/Projetos Visual Studio Code/Estruturas de Dados/Pilha
-# Build directory: G:/Dev/C++ Programs/Projetos Visual Studio Code/Estruturas de Dados/Pilha/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
